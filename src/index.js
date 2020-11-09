606f0244aaafb61e3770e08a7bd658b70e6c20a9
Eugene Verbovoi <63497261+verbovoi@users.noreply.github.com>
2020-11-09 23:07:30 +0200

    Добавляет весь функционал

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 //импорты
 import './styles.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import "@pnotify/core/dist/PNotify.css";
 import './fetchCountries';
 import { fetchCountry } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import listMarkupTmp from './templates/list-markup.hbs';
+import countryCardMarkup from './templates/country-card-markup.hbs';
+import { error, notice } from '@pnotify/core';
 
 //добавляем элементы ДОМ
 const refs = {
-  inputCountryEl: document.querySelector('#country-input')
+  inputCountryEl: document.querySelector('#country-input'),
+  containerEl: document.querySelector('.container')
 }
 
 //вешаем слушателя событий
@@ -14,15 +20,50 @@ refs.inputCountryEl.addEventListener('input', debounce(onSearchCountry, 500));
 
 //функция поиска страны
 function onSearchCountry(event) {
-    const searchQuery = event.target.value;
+  resetContainer();
+
+  const searchQuery = event.target.value;
 
   fetchCountry(searchQuery).
-    then(e => console.log(e));
-
+    then(renderCardMarkup).
+    catch(onFetchError);
 
 }
 
 
+//Функция рендера разметки на странице
+function renderCardMarkup(countries) {
+  if (countries.length > 10) {
+    notice({
+      title: 'Too many matches found. Please enter a more specific query!',
+      delay: 1000      
+});
+  }
+  else if (countries.length > 1) {
+      const markup = listMarkupTmp(countries);
+      refs.containerEl.innerHTML = markup;
+  }
+  else if(countries.length === 1) {
+    const markup = countryCardMarkup(...countries);
+    refs.containerEl.innerHTML = markup;
+  }
+  else {
+    error({
+      title: "Sorry! We can't find this country. Please try again.. ",
+      delay: 1000
+});
+  }
+};
+
+//Функция ошибки
+function onFetchError(error) {
+    // return console.error();
+}
+
+//Функция очистки разметки
+function resetContainer() {
+  refs.containerEl.innerHTML = '';
+}
 
 
 
